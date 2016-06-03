045fff062bd744cb025a728bf1dfd5e541d8cafd
Jéderson Gonçalves <jedersongmoura@gmail.com>
2016-06-03 11:11:09 -0300

    Criando e Adicionando o módulo shared - interface Calculadora

---
+++ shared/src/main/java/br/edu/ifpb/dac/shared/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.dac.shared;
+
+/**
+ *
+ * @author jederson
+ */
+public interface Calculadora {
+     
+    public int somar(int a, int b);
+    
+    public int subtrair(int a, int b);
+    
+    public int multiplicar(int a, int b);
+}
+
